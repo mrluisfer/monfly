6932f2581603d62d9ceebf0f08fc36fcf2b3b2c5
Luis Alvarez <mrluisfeer@gmail.com>
2025-05-27 18:58:28 -0600

    fix global prisma instance

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
-export const prismaClient = new PrismaClient();
+declare global {
+	var prisma: PrismaClient | undefined;
+}
+
+export const prismaClient = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+	global.prisma = prismaClient;
+}
 
 const saltRounds = 12;
 
