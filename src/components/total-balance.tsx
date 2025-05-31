ab8a057cb80332fd915d436b758bf546baf8458a
Luis Alvarez <mrluisfeer@gmail.com>
2025-05-30 18:19:36 -0600

    Implement TotalBalance component with editing functionality: add ability to edit and update user total balance, integrate mutation for server-side updates, and enhance UI with input and button components for better user interaction.

--- src/components/total-balance.tsx
+++ src/components/total-balance.tsx
@@ -1,34 +1,105 @@
 import { useQuery } from "@tanstack/react-query";
-import { DollarSign } from "lucide-react";
+import { Check, DollarSign, Edit2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 import { useRouteUser } from "~/hooks/use-route-user";
+import { useMutation } from "~/hooks/useMutation";
 import { userByEmailQueryOptions } from "~/queries/usersByEmail";
 import { formatCurrency } from "~/utils/formatCurrency";
+import { putUserTotalBalance } from "~/utils/putUserTotalBalance";
 import Card from "./card";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 import { Skeleton } from "./ui/skeleton";
 
 const TotalBalance = () => {
-	const user = useRouteUser();
+	const [isEditing, setIsEditing] = useState(false);
+	const [totalBalance, setTotalBalance] = useState(0);
 
-	if (!user.email) {
-		return <div>No user email</div>;
-	}
+	const user = useRouteUser();
+	if (!user.email) return <div>No user email</div>;
 
 	const { isPending, data, error } = useQuery(
 		userByEmailQueryOptions(user.email),
 	);
 
+	const putUserTotalBalanceMutation = useMutation({
+		fn: putUserTotalBalance,
+		onSuccess: async (ctx) => {
+			if (ctx.data?.error) {
+				toast.error(ctx.data.message);
+				return;
+			}
+			toast.success(ctx.data.message);
+			setIsEditing(false);
+		},
+	});
+
+	const handleEditClick = () => {
+		setIsEditing(true);
+	};
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const val = Number(e.target.value);
+		if (val >= 0 || e.target.value === "") {
+			setTotalBalance(val);
+		}
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		putUserTotalBalanceMutation.mutate({
+			data: {
+				totalBalance: Number(totalBalance),
+				email: user.email,
+			},
+		});
+	};
+
+	useEffect(() => {
+		if (data) setTotalBalance(data.totalBalance);
+	}, [data]);
+
+	if (error) return <div>Error</div>;
+
 	return (
-		<Card>
+		<Card
+			className="group"
+			title={
+				<div className="flex items-center justify-between">
+					Current Balance <DollarSign className="w-4 h-4 opacity-50" />
+				</div>
+			}
+		>
 			{isPending ? (
 				<Skeleton className="w-24 h-4" />
 			) : (
 				<>
-					<div className="flex items-center justify-between">
-						Current Balance <DollarSign className="w-4 h-4 opacity-50" />
-					</div>
-					<p className="text-2xl font-bold">
-						{formatCurrency(data?.totalBalance ?? 0, "MXN")}
-					</p>
+					{isEditing ? (
+						<form onSubmit={handleSubmit} className="flex items-center gap-2">
+							<Input
+								value={totalBalance.toString()}
+								onChange={handleInputChange}
+								type="number"
+								min={0}
+							/>
+							<Button variant="outline" size="icon" type="submit">
+								<Check size={12} />
+							</Button>
+						</form>
+					) : (
+						<p className="text-2xl font-bold flex items-center justify-between gap-2">
+							{formatCurrency(totalBalance ?? 0, "MXN")}
+							<Button
+								variant="outline"
+								size="icon"
+								className="group-hover:opacity-100 opacity-0"
+								onClick={handleEditClick}
+							>
+								<Edit2 size={12} />
+							</Button>
+						</p>
+					)}
 				</>
 			)}
 		</Card>
