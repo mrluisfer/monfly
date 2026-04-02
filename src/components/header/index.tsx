f9642f4a1153fdf2094e989d92b27e07b353911a
Luis Alvarez <mrluisfeer@gmail.com>
2026-04-02 17:13:44 -0600

    feat: update mobile header by removing SettingsDialog and adjusting layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -64,8 +64,8 @@ export const Header = () => {
           <HeaderNavigation />
         </nav>
         <div className="flex items-center justify-end gap-3">
+          <HideData />
           <ToggleDarkMode />
-          <SettingsDialog />
           <MobileHeaderSheetMenu />
         </div>
       </div>
